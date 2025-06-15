f76730db0c864ea6444208d8e63ed646bbb5082c
Mahmud Umarov <mahmud.umarov@gmail.com>
2025-06-16 03:07:02 +0500

    Исправлен chat_id на мой настоящий

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '8093981505:AAE2aoZhQ4RzZwEXBqforiZTFkr3vVyQNFc'
 SECRET_PATH = 'trapxyz123'  # You can customize this
-YOUR_CHAT_ID = '871664363'  # Replace after getting your chat ID
+YOUR_CHAT_ID = '290051130'  # Replace after getting your chat ID
