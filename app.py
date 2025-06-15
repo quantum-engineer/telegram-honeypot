f73edbb427dc92f9386f16b1e186c487ba1ae931
Mahmud Umarov <mahmud.umarov@gmail.com>
2025-06-16 02:34:27 +0500

    Add Flask run statement for Render

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def webhook():
     headers = dict(request.headers)
     text = json.dumps(data, indent=2)
 
-    message = f"🚨 ВХОД НА ХОУНИПОТ\nIP: {ip}\nHeaders: {headers.get('User-Agent')}\n\nPayload:\n{text[:1000]}"
+    message = f"🚨 ВХОД НА БОЧКУМЕДА\nIP: {ip}\nHeaders: {headers.get('User-Agent')}\n\nPayload:\n{text[:1000]}"
 
     requests.get(f"https://api.telegram.org/bot{config.BOT_TOKEN}/sendMessage", params={
         "chat_id": config.YOUR_CHAT_ID,
@@ -21,3 +21,8 @@ def webhook():
     })
 
     return "OK", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
